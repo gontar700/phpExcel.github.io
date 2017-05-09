bf13eddead1e7d30461385a86095bbab4e981c4c
gontar700 <danielgontar@gmail.com>
2017-05-09 14:38:24 +0300

    Add files via upload

---
+++ A.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: danielgontar
+ * Date: 5/9/17
+ * Time: 1:46 PM
+ */
+namespace daniel;
+/**
+ * A
+ *
+ * Copyright (c) 2006 - 2015 PHPExcel
+ *
+ *
+ *
+ * @category
+ * @package
+ * @copyright
+ * @license
+ * @version
+ */
+class A
+{
+    /**
+     * Unique ID
+     *
+     * @var string
+     */
+    private $stylesheet;
+
+    public function __construct($stylesheet)
+    {
+        $this->stylesheet = $stylesheet;
+
+    }
+    /*
+     * method p: var_dumps $this->stylesheet
+     * parameters:
+     */
+    public function p()
+    {
+        var_dump($this->stylesheet);
+    }
+}
